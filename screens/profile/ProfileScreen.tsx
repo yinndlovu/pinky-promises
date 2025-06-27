02173c6c8897aa73fe9cc063f03ca1c94e615ebd
yinndlovu <yinnndlovu@gmail.com>
2025-06-27 17:22:06 +0200

    Change a few things on the home screen

--- screens/profile/ProfileScreen.tsx
+++ screens/profile/ProfileScreen.tsx
@@ -91,7 +91,8 @@ const ProfileScreen = () => {
                 );
 
                 const mime = pictureResponse.headers["content-type"] || "image/jpeg";
-                const base64 = `data:${mime};base64,${encode(pictureResponse.data.buffer)}`;
+                const base64Data = Buffer.from(pictureResponse.data, "binary").toString("base64");
+                const base64 = `data:${mime};base64,${base64Data}`;
 
                 console.log("avatar uri", base64);
                 setAvatarUri(base64);
