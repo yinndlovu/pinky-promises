6d4df5cc66214454c873d2cd4c98df8fefea484e
yinndlovu <yinnndlovu@gmail.com>
2025-07-29 19:42:29 +0200

    Fix the update mood modal too

--- src/components/modals/selection/UpdateMoodModal.tsx
+++ src/components/modals/selection/UpdateMoodModal.tsx
@@ -207,9 +207,7 @@ const styles = StyleSheet.create({
     fontSize: 16,
   },
   cancelButton: {
-    backgroundColor: "transparent",
-    borderColor: "#e03487",
-    borderWidth: 2,
+    backgroundColor: "#393a4a",
     paddingVertical: 12,
     paddingHorizontal: 32,
     borderRadius: 8,
@@ -218,7 +216,7 @@ const styles = StyleSheet.create({
     marginLeft: 8,
   },
   cancelButtonText: {
-    color: "#e03487",
+    color: "#fff",
     fontWeight: "bold",
     fontSize: 16,
   },
