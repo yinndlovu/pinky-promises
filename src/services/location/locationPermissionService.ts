587ffa0d5fec1ac091d0670e8cbb67d8efc5cca4
yinndlovu <yinnndlovu@gmail.com>
2025-08-02 09:29:10 +0200

    Lower distance interval

--- src/services/location/locationPermissionService.ts
+++ src/services/location/locationPermissionService.ts
@@ -31,7 +31,7 @@ export async function startBackgroundLocationTracking() {
     await Location.startLocationUpdatesAsync(LOCATION_TASK_NAME, {
       accuracy: Location.Accuracy.Balanced,
       timeInterval: 5 * 60 * 1000,
-      distanceInterval: 100,
+      distanceInterval: 50,
       showsBackgroundLocationIndicator: true,
       foregroundService: {
         notificationTitle: "Location Service",
