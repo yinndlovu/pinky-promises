b1028d295a8b04f0ba25f49eb309a4e7d6dbb6f6
Yin <162073364+yinndlovu@users.noreply.github.com>
2025-08-15 04:49:48 +0200

    Oh la la

--- src/services/location/locationPermissionService.ts
+++ src/services/location/locationPermissionService.ts
@@ -31,7 +31,7 @@ export async function startBackgroundLocationTracking() {
 
     await Location.startLocationUpdatesAsync(LOCATION_TASK_NAME, {
       accuracy: Location.Accuracy.Balanced,
-      timeInterval: 20 * 60 * 1000,
+      timeInterval: 15 * 60 * 1000,
       distanceInterval: 100,
       showsBackgroundLocationIndicator: true,
       foregroundService: {
