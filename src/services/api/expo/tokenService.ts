e050575a17535ea7e0c946e0c4f9099a378c2eff
Yin <yinnndlovu@gmail.com>
2025-10-14 15:30:33 +0200

    refactor: remove the useless guard to token api call

--- src/services/api/expo/tokenService.ts
+++ src/services/api/expo/tokenService.ts
@@ -1,11 +1,7 @@
 import axios from "axios";
 import { BASE_URL } from "../../../configuration/config";
 
-export async function saveToken(authToken: string | null, token: string) {
-  if (!authToken) {
-    return;
-  }
-
+export async function saveToken(authToken: string, token: string) {
   const res = await axios.post(
     `${BASE_URL}/push-token/save`,
     { token },
